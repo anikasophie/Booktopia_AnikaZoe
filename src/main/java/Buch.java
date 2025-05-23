1ecbb08b4e9c45b108addb59f7808fc8e4fa1491
anikasophie <anika.starcke@student.hnu.de>
2025-05-23 20:16:50 +0200

    Ändern

--- src/main/java/Buch.java
+++ src/main/java/Buch.java
@@ -1,2 +1,5 @@
 public class Buch {
+    public static void main(String[] args) {
+        
+    }
 }
--- src/main/java/Buecherliste.java
+++ src/main/java/Buecherliste.java
@@ -1,2 +1,5 @@
 public class Buecherliste {
+    public static void main(String[] args) {
+
+    }
 }
