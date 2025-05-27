72e5ee59fd10971bba061d8c8729eaf44439d820
zoee-z <zziehmer@gmail.com>
2025-05-27 14:23:43 +0200

    GUI 1. Prototyp hinzugefügt

--- src/main/java/Buecherliste.java
+++ src/main/java/Buecherliste.java
@@ -1,5 +1,29 @@
-public class Buecherliste {
-    public static void main(String[] args) {
+import javax.swing.*;
+import java.awt.*;
+
+public class Buecherliste extends JFrame{
+    private JPanel jpPanel;
+    private JTextField jtTitelEingabe;
+    private JComboBox comboGenre;
+    private JRadioButton rbtnEinStern;
+    private JButton btnLöschen;
+    private JButton btnListeAnzeigen;
+    private JRadioButton rbtnZweiSterne;
+    private JRadioButton rbtnVierSterne;
+    private JRadioButton rbtnDreiSterne;
+    private JRadioButton rbtnFünfSterne;
+    private JButton btnSpeichern;
+
+    //Konstruktor
+    public Buecherliste() throws HeadlessException {
+        setTitle ("Meine Bücher"); //***Titel noch sehr unkreativ
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setSize(600,300);
+        setContentPane(jpPanel);
+        setVisible(true);
+        setResizable(false);
+
+
 
     }
 }
