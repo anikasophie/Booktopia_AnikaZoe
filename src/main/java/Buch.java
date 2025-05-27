aef6606c71072735e97d3051df80843e7fc00316
anikasophie <anika.starcke@student.hnu.de>
2025-05-27 19:06:25 +0200

    Neu

--- src/main/java/Buch.java
+++ src/main/java/Buch.java
@@ -1,9 +1,16 @@
 public class Buch {
     private String titel;
+    private boolean HatFortsetzung;
+    private String genre;
+    private int bewertung;
+
+    public Buch(String titel, boolean hatFortsetzung, String genre, int bewertung) {
+        this.titel = titel;
+        HatFortsetzung = hatFortsetzung;
+        this.genre = genre;
+        this.bewertung = bewertung;
+    }
 
     public static void main(String[] args) {
-
-
-
     }
 }
--- src/main/java/BuecherEingabe.java
+++ src/main/java/BuecherEingabe.java
@@ -1,9 +1,12 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class BuecherEingabe extends JFrame{
     private JPanel jpPanel;
     private JTextField jtTitelEingabe;
+    private JCheckBox cBoxFortsetzung;
     private JComboBox comboGenre;
     private JRadioButton rbtnEinStern;
     private JButton btnLöschen;
@@ -13,24 +16,43 @@ public class BuecherEingabe extends JFrame{
     private JRadioButton rbtnDreiSterne;
     private JRadioButton rbtnFünfSterne;
     private JButton btnSpeichern;
-    private JCheckBox checkBox1;
+
 
     //Konstruktor
     public BuecherEingabe() throws HeadlessException {
         setTitle ("Büchereingabe"); //***Titel noch sehr unkreativ
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setSize(600,300);
+        setSize(600,600);
         setContentPane(jpPanel);
         setVisible(true);
         setResizable(false);
 
+        ButtonGroup grupperiung = new ButtonGroup(); //Gruppierung der RadioButton so, dass immer nur einer ausgewählt werden kann
+        grupperiung.add(rbtnEinStern);
+        grupperiung.add(rbtnZweiSterne);
+        grupperiung.add(rbtnDreiSterne);
+        grupperiung.add(rbtnVierSterne);
+        grupperiung.add(rbtnFünfSterne);
 
+
+
+        btnLöschen.addActionListener(new ActionListener() { //Löschen Button, Eingaben/Auswhl wieder wie bei Start
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                jtTitelEingabe.setText("");
+                comboGenre.setSelectedIndex(0);
+                rbtnEinStern.setSelected(false);
+                rbtnZweiSterne.setSelected(false);
+                rbtnDreiSterne.setSelected(false);
+                rbtnVierSterne.setSelected(false);
+                rbtnFünfSterne.setSelected(false);
+            }
+        });
     }
 
-    //main
-
-
-
+    public static void main(String[] args) {
+        BuecherEingabe buecherEingabe = new BuecherEingabe();
+    }
 }
 
 
