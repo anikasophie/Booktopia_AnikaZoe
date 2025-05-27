18987d186d55babf5c916e959083ae948c30d67d
zoee-z <zziehmer@gmail.com>
2025-05-27 19:54:23 +0200

    Test

--- src/main/java/Buch.java
+++ src/main/java/Buch.java
@@ -1,12 +1,14 @@
+//Hier werden alle Informationen zu einem Buch gespeichert
+
 public class Buch {
     private String titel;
-    private boolean HatFortsetzung;
+    private boolean hatFortsetzung;
     private String genre;
     private int bewertung;
 
     public Buch(String titel, boolean hatFortsetzung, String genre, int bewertung) {
         this.titel = titel;
-        HatFortsetzung = hatFortsetzung;
+        this.hatFortsetzung = hatFortsetzung;
         this.genre = genre;
         this.bewertung = bewertung;
     }
